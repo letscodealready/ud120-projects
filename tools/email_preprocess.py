a065bc09e730a0629fb087bddc2d12650933a099
letscodealready <joseph.k.boadi@gmail.com>
2016-05-09 15:18:49 +0100

    Update email_preprocess.py
    Python 3 doesn't use cPickle
    and has to be read as bytes

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import pickle
-import cPickle
+#import cPickle
 import numpy
 
 from sklearn import cross_validation
@@ -29,13 +29,13 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "r")
-    authors = pickle.load(authors_file_handler)
-    authors_file_handler.close()
+    with open(authors_file, "rb") as authors_file_handler:
+        authors = pickle.load(authors_file_handler)
+        authors_file_handler.close()
 
-    words_file_handler = open(words_file, "r")
-    word_data = cPickle.load(words_file_handler)
-    words_file_handler.close()
+    with open(words_file, "r") as words_file_handler:
+        word_data = cPickle.load(words_file_handler)
+        words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
@@ -59,7 +59,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
     ### info on the data
-    print "no. of Chris training emails:", sum(labels_train)
-    print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
+    print("no. of Chris training emails:", sum(labels_train))
+    print ("no. of Sara training emails:", len(labels_train)-sum(labels_train))
     
     return features_train_transformed, features_test_transformed, labels_train, labels_test
